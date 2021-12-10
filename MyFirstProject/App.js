125b509848dbbf44e7180a3b8ae76b2e349caae3
Daniel Valiente Muñoz <daniel.ma42585@cesurformacion.com>
2021-12-10 13:48:21 +0100

    V1.02

--- MyFirstProject/App.js
+++ MyFirstProject/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View, FlatList,Image,ScrollView,SafeAreaView,StyleSheet, TouchableOpacity, TextInputBase,TextInput,Button } from 'react-native';
+import { Text, View, FlatList, Image, ScrollView, SafeAreaView, StyleSheet, TextInput, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -12,40 +12,90 @@ const Tab = createBottomTabNavigator();
 
 
 
-
 const HomeStack = createNativeStackNavigator();
 
 function listado() {
   return (
-    <HomeStack.Navigator initialRouteName="Historia">
-      <HomeStack.Screen name="Listado" component={ListScreen} options={{ headerStyle: { backgroundColor: '#46350C' } }} />
-      <HomeStack.Screen name="Historia" component={HistoryScreen} options={{ headerStyle: { backgroundColor: '#46350C' } }} />
+    <HomeStack.Navigator initialRouteName="Listado">
+      <HomeStack.Screen name="Listado" component={ListScreen} options={{ headerStyle:{backgroundColor:'#625204' }}} />
+      <HomeStack.Screen name="Filtro" component={FiltroScreen} options={{ headerStyle:{backgroundColor:'#625204'} }} />
     </HomeStack.Navigator>
 
   );
 }
-const InfoStack = createNativeStackNavigator();
 
-function Info({navigation}) {
+
+function ListScreen({ navigation }) {
+  const [number, onChangeNumber] = React.useState(null);
+  return (
+    <View style={styles.view} >
+      <Text>Busca aqui tus usuarios por edad</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="EDAD"
+        keyboardType="numeric"
+      />
+      <Button
+        title="Search"
+        onPress={() => navigation.navigate('Filtro', { number: number }
+        )}
+      />
+    </View>
+
+  )
+}
+function FiltroScreen({ navigation, route }) {
+  const users = [
+    { id: 1, name: 'Antonio Morlanes', age: 34, sex: 'Varón' },
+    { id: 2, name: 'Margarita Fuentes', age: 29, sex: 'Mujer' },
+    { id: 4, name: 'Manuel Machado', age: 51, sex: 'Varón' },
+    { id: 5, name: 'Cai Lun', age: 81, sex: 'Varón' },
+    { id: 6, name: 'Manuela Aparicia', age: 19, sex: 'Varón' },
+    { id: 7, name: 'Manuel Lara', age: 20, sex: 'Varón' },
+    { id: 9, name: 'Álvaro Andrade', age: 43, sex: 'Varón' },
+    { id: 10, name: 'Ángel Andrade', age: 23, sex: 'Varón' },
+    { id: 11, name: 'Araceli Castillo', age: 61, sex: 'Mujer' },
+    { id: 12, name: 'Sara Sacristán', age: 49, sex: 'Mujer' },
+    { id: 13, name: 'Esther Arroyo', age: 18, sex: 'Mujer' },
+    { id: 14, name: 'Martina Danta', age: 45, sex: 'Mujer' },
+    { id: 15, name: 'Julia Praena', age: 38, sex: 'Mujer' },
+    { id: 16, name: 'Pedro Flecha', age: 53, sex: 'Varón' },
+    { id: 17, name: 'Miguel Berral', age: 60, sex: 'Varón' },
+    { id: 18, name: 'Lorena Aparicio', age: 53, sex: 'Mujer' },
+    { id: 19, name: 'David Toral', age: 61, sex: 'Varón' },
+    { id: 20, name: 'Daniel Cifuentes', age: 52, sex: 'Varón' }
+  ]
+
+  function filtrar({ item }) {
+
+    if (item.age == route.params.number) {
+      return <View style={styles.view}>
+        <Text>{item.name}</Text>
+        <Text>{item.age}</Text>
+      </View>
+
+    }
+  }
+
   return (
 
     <View style={styles.view}>
-      <Text>Busca aqui tus usarios por edad</Text>
-      <Text>Edad</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Edad"
-        keyboardType="numeric"
+      <FlatList
+        data={users}
+        renderItem={filtrar}
+        keyExtractor={item => item.id}
       />
-       <Button
-        title="Buscar"
-        onPress={() => navigation.navigate('Listado')}
+      <Button
+        title="Volver"
+        onPress={() => navigation.navigate('Listado'
+        )}
       />
-
-      </View>
+    </View>
   )
-  
 }
+
 
 function HistoryScreen() {
 
@@ -53,50 +103,26 @@ function HistoryScreen() {
 
     <SafeAreaView style={styles.view}>
       <ScrollView style={styles.scrollview}>
-      <Text>Bienvenido a la historia de los dispositivos, este es primer móvil que salió al mercado</Text>
-      <Image source={{uri: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/02/16155009/Motorola-DynaTAC-SF-1024x768.jpg'}}
-       style={{width: 200, height: 200}} />
-      <Text>Este es el movil mas vendido hasta la actualidad</Text>
-      <Image source={{uri: 'https://m.media-amazon.com/images/I/41E2HB43R6L._AC_SS450_.jpg'}}
-       style={{width: 200, height: 200}} />
-      <Text>Este es el último Iphone</Text>
-      <Image source={{uri: 'https://static.k-tuin.com/media/catalog/product/cache/1/image/0dc2d03fe217f8c83829496872af24a0/c/o/comprar-nuevo-iphone-13-pro-max-color-oro-128gb.jpg'}}
-       style={{width: 200, height: 200}} />
-      <Text>Y este el último modulo que ha sacado Samsung</Text>
-      <Image source={{uri: 'http://img.pccomponentes.com/articles/34/348294/1130-samsung-galaxy-s21-5g-128gb-rosa-libre-especificaciones.jpg'}}
-       style={{width: 200, height: 200}} />
-     </ScrollView>
+        <Text>Bienvenido a la historia de los dispositivos, este es primer móvil que salió al mercado (Motorola DynaTAC 8000X )</Text>
+        <Image source={{ uri: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2017/02/16155009/Motorola-DynaTAC-SF-1024x768.jpg' }}
+          style={{ width: 200, height: 200 }} />
+        <Text>Este es el movil mas vendido hasta la actualidad (NOKIA 1100)</Text>
+        <Image source={{ uri: 'https://m.media-amazon.com/images/I/41E2HB43R6L._AC_SS450_.jpg' }}
+          style={{ width: 200, height: 200 }} />
+        <Text>Este es el último Iphone (IPHONE 13 PRO MAX)</Text>
+        <Image source={{ uri: 'https://static.k-tuin.com/media/catalog/product/cache/1/image/0dc2d03fe217f8c83829496872af24a0/c/o/comprar-nuevo-iphone-13-pro-max-color-oro-128gb.jpg' }}
+          style={{ width: 200, height: 200 }} />
+        <Text>Y este el último modulo que ha sacado Samsung (SAMSUNG S21)</Text>
+        <Image source={{ uri: 'http://img.pccomponentes.com/articles/34/348294/1130-samsung-galaxy-s21-5g-128gb-rosa-libre-especificaciones.jpg' }}
+          style={{ width: 200, height: 200 }} />
+      </ScrollView>
     </SafeAreaView>
   );
 
 }
-function ListScreen({ navigation }) {
 
-  function pintar({ item }) {
-    return (
-      <View style={styles.view} >
-       <Text>Busca aqui tus usuarios por edad</Text>
-       <Text>Edad</Text>
-       <TextInputBase></TextInputBase>
-        <TouchableOpacity
-          style={styles.touchable}
-          onPress={() => navigation.navigate('Details', { item: item })} >
-          <Text  >{item.nombre}</Text>
-        </TouchableOpacity>
-      </View>
 
-    )
-  }
-  return (
 
-    <View>
-      <FlatList
-        data={DATA}
-        renderItem={pintar}
-        kayExtractor={item => item.id} />
-    </View>
-  );
-}
 
 
 const styles = StyleSheet.create({
@@ -125,8 +151,8 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   scrollview: {
-  
-    marginHorizontal:20,
+
+    marginHorizontal: 20,
   }
 });
 
@@ -151,8 +177,8 @@ export default function App() {
           tabBarInactiveTintColor: 'grey',
         })}
       >
-        <Tab.Screen name="Historia" component={listado} options={{ headerStyle: { backgroundColor: '#625204' } }} />
-        <Tab.Screen name="Listado" component={Info} options={{ headerStyle: { backgroundColor: '#625204' } }} />
+        <Tab.Screen name="Historia" component={HistoryScreen} options={{ headerStyle: { backgroundColor: '#625204' } }} />
+        <Tab.Screen name="Listado" component={listado} options={{ headerShown: false }} />
 
       </Tab.Navigator>
     </NavigationContainer>
